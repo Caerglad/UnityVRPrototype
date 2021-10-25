3118eb37415d2c5a80247ecb52a954bb4fa47fae
Krzysztof Michoński <michonskikrzysztof@gmail.com>
2021-10-25 19:20:36 +0200

    feat: main menu
    Signed-off-by: Krzysztof Michoński <michonskikrzysztof@gmail.com>

---
+++ Assets/Scenes/MainMenu/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scenes.MainMenu
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            Debug.Log("Trying to load Scene with name: " + sceneName);
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
